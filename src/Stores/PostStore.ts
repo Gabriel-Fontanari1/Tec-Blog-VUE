916c0f90e111f2d4a2e4614b17e915fd02daaaef
Gabriel <gabriel.fontanari@owntec.com.br>
2026-05-11 11:05:14 -0300

    Api de imagens

--- src/Stores/PostStore.ts
+++ src/Stores/PostStore.ts
@@ -2,20 +2,27 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const usePostStore = defineStore('post', () => {
-    // Array vazio
     const dataCards = ref<any[]>([])
     const loading = ref(false)
 
-    // Buscar os dados da api
     const getPosts = async () => {
         loading.value = true
         try {
             const response = await fetch('https://dummyjson.com/posts')
             const data = await response.json()
+            
+            // Percorre os posts para adiconar uma propriedade imagem
+            // data.post é o array real de 30 objs
+            dataCards.value = data.posts.slice(0, 16).map((post: any) => {
+                // Any -> typescript verifica os tipos, any indica que nao vai ser informado a tipagem ainda
+                return {
+                    ...post,
+                    // Id para gerar 1 imagem unica para cada
+                    // Largura de 400 por 250
+                    image: `https://picsum.photos/seed/${post.id}/400/250`
+                }
+            })
 
-            // A API retorna um objeto. O array de posts está dentro da chave .posts
-            // Puxa os 16 primeiros valores / 16 valores por pagina
-            dataCards.value = data.posts.slice(0, 16)
         } catch (error) {
             console.error("Erro ao buscar posts:", error)
         } finally {
