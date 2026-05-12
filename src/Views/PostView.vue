65694d8d0fd766bc456bdd3069442ad0697312e2
Gabriel <gabriel.fontanari@owntec.com.br>
2026-05-12 16:49:36 -0300

    Layout do page view finalizado e botão home pronto

--- src/Views/PostView.vue
+++ src/Views/PostView.vue
@@ -28,30 +28,35 @@ watch(() => route.params.id, loadPost)
 	</div>
 
 	<div class="PageContainer">
-		<div class="MainContainerView" v-if="post">
-			<div class="ImageContainer">
-				<div class="Img"></div>
-				<img :src="post.image" :alt="post.title">
-				
-				<div class="LikesContainer">
-					<div class="Like">
-						<p>{{ post.reactions?.likes }}</p>
-					</div>
 
-					<div class="Dislikes">
-						<p>{{ post.reactions?.dislikes }}</p>
+		<div class="MainContainerView" v-if="post">
+			<div class="PostContainer">
+
+				<div class="TextsContainer">
+					<h1>{{ post.title }}</h1>
+					<p>{{ post.body }}</p>
+				</div>
+				
+				<div class="ImageContainer">
+					<div class="Img"></div>
+					<img :src="post.image" :alt="post.title">
+
+					<div class="LikesContainer">
+						<div class="Like">
+							<i class="material-icons">thumb_up</i>
+							<p>{{ post.reactions?.likes }}</p>
+						</div>
+
+						<div class="Dislikes">
+							<i class="material-icons">thumb_down</i>
+							<p>{{ post.reactions?.dislikes }}</p>
+						</div>
 					</div>
 				</div>
 			</div>
-			
-			
-
-			<div class="TextsContainer">
-				<h1>{{ post.title }}</h1>
-				<p>{{ post.body }}</p>
-			</div>
 
 			<div class="CommentsContainer">
+				<h2>Comment Section</h2>
 				<div v-for="comment in post.comments" :key="comment.id" class="Usr">
 					<img :src="comment.user.image" :alt="comment.user.username">
 					<div class="InfoUsrs">
@@ -59,15 +64,12 @@ watch(() => route.params.id, loadPost)
 						<p>{{ comment.body }}</p>
 					</div>
 
-					<div class="ComementLikes">
+
 						<div class="LikeComment">
+							<i class="material-icons">thumb_up</i>
 							<p>{{ comment.likes }}</p>
 						</div>
 
-						<div class="DislikesComment">
-							<p>{{ comment.dislikes }}</p>
-						</div>
-					</div>
 				</div>
 			</div>
 
@@ -96,14 +98,21 @@ watch(() => route.params.id, loadPost)
 }
 
 .PageContainer {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 	padding-top: 17rem;
 	width: 100%;
 }
 
 .MainContainerView{
+	background-color: #0B4A72;
 	display: flex;
 	flex-direction: column;
 	color: white;
+	margin-inline: 2rem;
+	border-radius: 1rem;
 }
 
 .ImageContainer{
@@ -112,12 +121,111 @@ watch(() => route.params.id, loadPost)
 	align-items: end;
 	align-self: flex-end;
 	gap: 1rem;
+	margin-inline: 3rem;
 }
 
-.LikesContainer{
+.ImageContainer img{
+	border-radius: 1rem;
+}
+
+.LikesContainer {
+	display: flex;
+	align-items: center;
+	justify-content: flex-end;
+	gap: 1rem;
+	margin-bottom: 1rem;
+}
+
+.Like,
+.Dislikes {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	gap: 0.5rem;
+	height: 2.5rem;
+	min-width: 5rem;
+	padding: 0 1rem;
+	border-radius: 0.7rem;
+	background-color: #0B4A72;
+	color: white;
+}
+
+.Like p,
+.Dislikes p {
+	margin: 0;
+}
+
+.material-icons {
+	font-size: 1.2rem;
+}
+
+.PostContainer{
 	display: flex;
 	flex-direction: row;
-	justify-content: end;
+	margin: 2rem;
+	border-radius: 1rem;
+	background-color: #093C5D;
+}
+
+.TextsContainer{
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	align-content: center;
+	justify-content: center;
+	justify-items: center;
+	align-self: center;
+	gap: 1rem;
+	width: 100%;
+}
+
+.PostContainer p, h1{
+	width: 70%;
+}
+
+.Usr{
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	gap: 1rem;
+	margin-inline: 13rem;
+	margin-block: 2rem;
+	border-radius: 1rem;
+	padding: 0.5rem;
+	background-color: #093C5D;
+}
+
+.Usr img{
+	border-radius: 1rem;
+}
+
+.InfoUsrs{
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	gap: 1rem;
+	flex: 1;
+	min-width: 0;
+}
+
+.CommentsContainer{
+	background-color: #0B4A72;
+}
+
+.CommentsContainer h2{
+	margin-inline: 13rem;
+}
+
+.LikeComment{
+	display: flex;
+	flex-direction: row;
+	align-self: center;
+	margin-left: auto;
+	border-radius: 1rem;
+	padding: 1rem;
+	background-color: #0B4A72;
+	gap: 1rem;
+	flex: 0 0 auto;
 }
 
 </style>
